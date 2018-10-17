c07cdf38d4d1180fe804f23e4748d4af7b548a0e
Resky Javieri <reskyjavier@gmail.com>
2018-10-17 10:34:53 +0700

    AdminEventDetailVC

---
+++ Bantu/Bantu/Controller/Admin/Event/AdminEventDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AdminEventDetailViewController.swift
+//  Bantu
+//
+//  Created by Resky Javieri on 17/10/18.
+//  Copyright © 2018 Resky Javieri. All rights reserved.
+//
+
+import UIKit
+
+class AdminEventDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+
+
+}
